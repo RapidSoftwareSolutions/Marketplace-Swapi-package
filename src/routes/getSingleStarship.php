a170328b9c20c222f3faffcf52772e8056000558
Stefan Skliarov <serg.osipchuk@rapidapi.com>
2017-06-09 20:15:59 +0300

    quick fix

--- src/routes/getSingleStarship.php
+++ src/routes/getSingleStarship.php
@@ -4,7 +4,7 @@ $app->post('/api/Swapi/getSingleStarship', function ($request, $response, $args)
 
     //checking properly formed json
     $checkRequest = $this->validation;
-    $validateRes = $checkRequest->validate($request, ['startshipId']);
+    $validateRes = $checkRequest->validate($request, ['starshipId']);
     if (!empty($validateRes) && isset($validateRes['callback']) && $validateRes['callback'] == 'error') {
         return $response->withHeader('Content-type', 'application/json')->withStatus(200)->withJson($validateRes);
     } else {
